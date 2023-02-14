2dc60c37ec81d9eb7cb070965972bc9d0a30b293
NMech <124092871+NMech@users.noreply.github.com>
2023-02-14 18:50:34 +0200

    Re upload all files

--- src/import_pys/main_funcs.py
+++ src/import_pys/main_funcs.py
@@ -1,25 +1,37 @@
 # -*- coding: utf-8 -*-
+import os
+import pickle
 import time
 import numpy as np
-from sklearn.model_selection import cross_val_score
+from   sklearn.model_selection import cross_val_score
 #%%
-def Classifier_Fit(classifier, X_train, y_train, Cv, clf_txt):
+def Classifier_Fit(classifier, X_train, y_train, Cv, clf_txt, filepathOut):
     """
-    Implementation of classifier fitting.
+    Implementation of classifier fitting.\n
+    Keyword arguments:\n
+        classifier : sklearn classifier object.\n
+        X_train    : Training dataset [pd.DataFrame].\n
+        y_train    : Labels of training dataset [pd.Series].\n 
+        Cv         : Number of cross-validation folds [int].\n
+        clf_txt    : Text to print in screen (also used for naming the model).\n
+        filepathOut: Filepath where the models are saved.\n
     """
     t1 = time.time()
     classifier.fit(X_train, y_train)
     classifier_CV_score = cross_val_score(classifier, X_train, y_train, scoring="accuracy", cv=Cv, n_jobs=-1)
     score = round(np.mean(classifier_CV_score),3)
+    Save_Classifier(classifier, filepathOut, clf_txt)
     t2 = time.time()
-    msg = f"{clf_txt:30} {round(t2-t1,4)}s"
+    msg = f"{clf_txt:40} |{round(t2-t1,4)}s"
     print(msg)
-    return classifier, score
 
+    return classifier, score
 
 def return_q1_q3(df):
     """
-    Valid only for this exercise (not fgeneralised).
+    Valid only for this exercise (not generalised).\n
+    Keyword arguments:\n
+        df : pd.DataFrame.\n
     """
     df    = df.copy()
     q1_q3 = {}
@@ -37,5 +49,12 @@ def return_q1_q3(df):
         df.loc[df[col]<=Lower_fence, col] = median
         
     return df
-        
+
+def Save_Classifier(classifier, filepathOut, filename):
+    """
+    Used for saving the trained model using pickle.\n
+    """
+    fileOut = os.path.join(filepathOut, filename)
+    pickle.dump(classifier, open(fileOut, "wb"))
     
+    return None
